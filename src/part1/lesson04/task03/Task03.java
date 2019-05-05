f2fdd363a0706334a1052c3633ce0e42aab1a337
LV4m3J <exaliburespada@gmail.com>
2019-05-05 13:31:55 +0300

    03.05.2019 ДЗ4

--- src/part1/lesson04/task03/Task03.java
+++ src/part1/lesson04/task03/Task03.java
@@ -3,7 +3,3 @@ package part1.lesson04.task03;
 public class Task03 {
 
 }
-
-//TODO Доработать классы FirstMathBox и ObjectBox таким образом, чтобы FirstMathBox был наследником ObjectBox.
-// Необходимо сделать такую связь, правильно распределить поля и методы. Функциональность в целом должна сохраниться.
-// При попытке положить Object в FirstMathBox должно создаваться исключение.
