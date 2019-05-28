cf0479bbc2870c2c2d881ab5fc252b869526296a
LV4m3J <exaliburespada@gmail.com>
2019-05-28 19:59:42 +0300

    28.05.2019 ДЗ8

--- src/part1/lesson08/task01/Task01.java
+++ src/part1/lesson08/task01/Task01.java
@@ -1,7 +1,12 @@
 package part1.lesson08.task01;
 
 public class Task01 {
-
+    public static void main(String[] args) {
+        Serial serial = new Serial();
+        Library library1 = new Library("Books1, Books2",new int[]{1,2,3,4,5},true);
+        serial.serialize(library1,"src\\part1\\lesson08\\task01\\library1.bin");
+        System.out.println(serial.deSerialize("src\\part1\\lesson08\\task01\\library1.bin"));
+    }
 }
 
 //TODO Необходимо разработать класс, реализующий следующие методы:
