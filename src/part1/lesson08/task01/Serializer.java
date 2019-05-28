11e0a803df3ce7d819a1e0bbb28b923159895f4a
LV4m3J <exaliburespada@gmail.com>
2019-05-28 20:20:31 +0300

    28.05.2019 ДЗ8

--- src/part1/lesson08/task01/Serializer.java
+++ src/part1/lesson08/task01/Serializer.java
@@ -1,7 +1,17 @@
 package part1.lesson08.task01;
 
 public interface Serializer {
+    /**
+     * serialize - класс сериализации
+     * @param object - объект для сериализации
+     * @param file - имя файла, можнос указать путь
+     */
     void serialize(Object object, String file);
 
-    Object deSerialize(String path);
+    /**
+     * deSerialize - класс десериализации
+     * @param file - путь до файла
+     * @return - возвращает объект десериализации
+     */
+    Object deSerialize(String file);
 }
--- src/part1/lesson08/task01/Task01.java
+++ src/part1/lesson08/task01/Task01.java
@@ -8,9 +8,3 @@ public class Task01 {
         System.out.println(serial.deSerialize("src\\part1\\lesson08\\task01\\library1.bin"));
     }
 }
-
-//TODO Необходимо разработать класс, реализующий следующие методы:
-//void serialize (Object object, String file);
-//Object deSerialize(String file);
-//Методы выполняют сериализацию объекта Object в файл file и десериализацию объекта из этого файла.
-// Обязательна сериализация и десериализация "плоских" объектов (все поля объекта - примитивы, или String).
