d38a8d671611c682cc8ad1d40456edb5e0414194
cocain <exaliburespada@gmail.com>
2019-06-10 23:29:54 +0300

    10.06.2019

--- src/part5/lesson17/task02/Task02.java
+++ src/part5/lesson17/task02/Task02.java
@@ -1,6 +1,22 @@
 package part5.lesson17.task02;
 
+import part5.lesson17.datasource.MyDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Task02 {
+
+    Connection dbConnection;
+
+    public void dbConnection() throws SQLException {
+        DataSource ds = MyDataSource.getPostgresDataSource();
+        dbConnection = ds.getConnection();
+    }
+    public int executeQuery(String query) throws SQLException {
+        return dbConnection.createStatement().executeUpdate(query);
+    }
 }
 
 //TODO Использую Spy и Mockito создать заглушки для интерфейса JDBC
