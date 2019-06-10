4ce77ba301bed055c0dd3f5db99538dc190bc1c2
cocain <exaliburespada@gmail.com>
2019-06-11 00:15:37 +0300

    10.06.2019

---
+++ test/java/part5/lesson17/task02/Task02Test.java
@@ -0,0 +1,36 @@
+package part5.lesson17.task02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task02Test {
+
+    @InjectMocks private Task02 dbConnection;
+    @Mock private Connection mockConnection;
+    @Mock private Statement mockStatement;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testMockDBConnection() throws Exception {
+        Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+        Mockito.when(mockConnection.createStatement().executeUpdate(Mockito.any())).thenReturn(1);
+        int value = dbConnection.executeQuery("");
+        Assertions.assertEquals(value, 1);
+        Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
+    }
+
+}
